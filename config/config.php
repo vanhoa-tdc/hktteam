d00d01a1d08284388ec9b0a4306887248e3ccbe4
vanhoa-tdc <hoa96610@gmail.com>
2025-10-26 14:14:52 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "localhost";
+$configDB["host"] 		= "hktteam.mysql.database.azure.com";
 $configDB["database"]	= "computer_store";
-$configDB["username"] 	= "root";
-$configDB["password"] 	= "";
-define("HOST", "localhost");
+$configDB["username"] 	= "hktteam";
+$configDB["password"] 	= "tdc@12345";
+define("HOST", "hktteam.mysql.database.azure.com");
 define("DB_NAME", "computer_store");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_USER", "hktteam");
+define("DB_PASS", "tdc@12345");
 define('ROOT', dirname(dirname(__FILE__) ) );
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
 define("BASE_URL", "http://".$_SERVER['SERVER_NAME']);//dia chi website
